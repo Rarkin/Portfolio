5cefa03f819fd30ab5d4d2d797762bcfd03dc49b
Karl <karlgraves28@gmail.com>
2023-01-18 18:51:41 +1100

    changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,34 @@ const NavBar = () => {
         <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300">
             <div>
                 <img src={Logo} alt='Logo image' style={{width: '100px'}} />
-            </div>            
+            </div>
+            <div>
+                {/* menu */}
+                <ul className="flex">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Skills</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            {/* hamburger */}
+            <div className="hidden">
+                <FaBars />
+            </div>
+        
+            {/* mobile menu */}
+            <ul className="hidden">
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Skills</li>
+                    <li>Contact</li>
+            </ul>
+
+            {/* Social Icons */}
+            <div></div>
+
         </div>
     )
 }
