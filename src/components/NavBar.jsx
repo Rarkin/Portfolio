55b0d771b81c0eb889ccb6cc08f127d6260c5722
Karl <karlgraves28@gmail.com>
2023-01-20 14:37:38 +1100

    added git hub to social icons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -44,6 +44,12 @@ const handleClick = () => setNav(!nav)
                             LinkedIn <FaLinkedin size={30}/>
                         </a>
                     </li>
+                    <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333]">
+                        <a className="flex justify-between items-center w-full text-gray-300"
+                        href="/">
+                            Github <FaGithub size={30}/>
+                        </a>
+                    </li>
                 </ul>
             </div>
 
