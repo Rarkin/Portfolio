a23511efde5a8e9cccb55e24858b8e7a99c93bf4
Karl <karlgraves28@gmail.com>
2023-01-24 18:36:49 +1100

    finished project component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,23 +2,50 @@ import React from 'react'
 
 import TempProjectImg from '../assests/TempProjectImg.png'
 
+import ReactImg from '../assests/react.png'
+import JavaScript from '../assests/javascript.png'
+
 function Projects() {
   return (
     <div name='projects' className='w-full h-full bg-gradient-to-r from-[#0f2027] via-[#203a43] to-[#2c5364] ...'>
         
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 text-[#ccd6f6] border-purple-600'>Projects</p>
                 <p className='py-4 text-[#ccd6f6]'>// Check out some of my projects!</p>
             </div>
 
-            <div style={{backgroundImage: `url(${TempProjectImg})`}} className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-                <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                    
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+
+                {/* Project Card */}
+                <div style={{backgroundImage: `url(${TempProjectImg})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>                    
                     {/* hover effects */}                    
                     <div className='opacity-0 group-hover:opacity-100'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
-                            React JS Application
+                            Project Name Here
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                        <div className='grid grid-cols-2'>
+                            <img className='w-12 py-3 mx-auto flex justify-center items-center' src={ReactImg} alt='React Icon' />
+                            <img className='w-12 py-3 mx-auto flex justify-center items-center' src={JavaScript} alt='React Icon' />
+                        </div>
+                    </div>
+                </div>
+
+                {/* Project Card */}
+                <div style={{backgroundImage: `url(${TempProjectImg})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>                    
+                    {/* hover effects */}                    
+                    <div className='opacity-0 group-hover:opacity-80'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Project Name Here
                         </span>
                         <div className='pt-8 text-center'>
                             <a href='/'>
@@ -29,8 +56,80 @@ function Projects() {
                             </a>
                         </div>
                     </div>
-
                 </div>
+
+                {/* Project Card */}
+                <div style={{backgroundImage: `url(${TempProjectImg})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>                    
+                    {/* hover effects */}                    
+                    <div className='opacity-0 group-hover:opacity-80'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Project Name Here
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Project Card */}
+                <div style={{backgroundImage: `url(${TempProjectImg})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>                    
+                    {/* hover effects */}                    
+                    <div className='opacity-0 group-hover:opacity-80'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Project Name Here
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Project Card */}
+                <div style={{backgroundImage: `url(${TempProjectImg})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>                    
+                    {/* hover effects */}                    
+                    <div className='opacity-0 group-hover:opacity-80'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Project Name Here
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Project Card */}
+                <div style={{backgroundImage: `url(${TempProjectImg})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>                    
+                    {/* hover effects */}                    
+                    <div className='opacity-0 group-hover:opacity-80'>
+                        <span className='text-2xl font-bold text-white tracking-wider'>
+                            Project Name Here
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                            </a>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
             </div>
 
         </div>
