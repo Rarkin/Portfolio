da9c9a23984e2bdcc5284628b3cf20367ccc7888
Karl <karlgraves28@gmail.com>
2023-01-25 14:50:37 +1100

    added to hero section description

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,6 +12,7 @@ const Hero = () => {
             <h1 className='sm:text-4xl text-7xl font-bold text-purple-600'>Karl Graves</h1>
             <h2 className='sm:text-4xl text-7xl text-[#ccd6f6]'>I'm a Full Stack Developer</h2>
             <p className='text-[#ccd6f6] py-4 max-w-700'>I have a passion for games which led me to learn how to code. I have experience creating games using Unity and Unreal Engine, utilizing languages such as C# and C++. I am now transitioning into full stack development, expanding my skillset to include technologies like React, JavaScript, HTML, CSS, and Node.js. I am excited to continue learning and growing in this field, and am eager to apply my skills to real-world projects.</p>
+            <p className='text-[#ccd6f6] py-4 max-w-700'>Check out my projects below! also feel free to check out my Linkedin, Github & Resume located on the sidebar. If you like what you see shoot me an email also located on the sidebar or send a message through my contact section.</p>
             <div>
               <Link to="projects" smooth={true} duration={500}>
                 <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-purple-600 hover:border-purple-600'>
