30352c923f15af9e861f6569a70a9526e9f0e05c
Karl <karlgraves28@gmail.com>
2023-02-08 13:25:42 +1100

    added first project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import TempProjectImg from '../assests/TempProjectImg.png'
+import JustLift from '../assests/just-lift-high-resolution-logo-color-on-transparent-background.png'
 
 import ReactImg from '../assests/react.png'
 import JavaScript from '../assests/javascript.png'
@@ -19,23 +20,22 @@ function Projects() {
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
 
                 {/* Project Card */}
-                <div style={{backgroundImage: `url(${TempProjectImg})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>                    
+                <div style={{backgroundImage: `url(${JustLift})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>                    
                     {/* hover effects */}                    
                     <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='font-bold text-white tracking-wider'>
-                            Project Name Here
+                        <span className='font-bold text-white tracking-wider flex justify-center items-center'>
+                            Just Lift
                         </span>
                         <div className='pt-8 text-center'>
-                            <a href='/'>
+                            <a href='https://justliftapp.netlify.app' target="_blank">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
-                            <a href='/'>
+                            <a href='https://github.com/Rarkin/My-Fitness-App' target="_blank">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
-                        <div className='grid grid-cols-2'>
+                        <div className='grid'>
                             <img className='w-12 py-3 mx-auto flex justify-center items-center' src={ReactImg} alt='React Icon' />
-                            <img className='w-12 py-3 mx-auto flex justify-center items-center' src={JavaScript} alt='React Icon' />
                         </div>
                     </div>
                 </div>
